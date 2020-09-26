f8ed2993484ea6e0615152dfdf192735a41a1c11
Ksenia Sidorova <nekfi@mail.ru>
2020-09-26 22:03:51 +0300

    Второе задание

--- src/js/__tests__/advanced.test.js
+++ src/js/__tests__/advanced.test.js
@@ -1,4 +1,5 @@
 import mySort from '../advanced';
+import skill from '../advanced__1'
 
 test('obj', () => {
     const received = mySort({name: 'мечник', health: 10, level: 2, attack: 80, defence: 40}, ["name", "level"]);
@@ -11,4 +12,45 @@ test('obj', () => {
       ]
 
       expect(received).toEqual(expected);
+});
+
+test('skill', () => {
+    const received = skill({
+        name: 'Лучник',
+        type: 'Bowman',
+        health: 50,
+        level: 3,
+        attack: 40,
+        defence: 10,
+        special: [
+          {
+            id: 8,
+            name: 'Двойной выстрел',
+            icon: 'http://...',
+            description: 'Двойной выстрел наносит двойной урон'
+          }, 
+          {
+            id: 9,
+            name: 'Нокаутирующий удар',
+            icon: 'http://...'
+          }
+        ]	
+      });
+
+      const expected = [
+        {
+            id: 8,
+            name: 'Двойной выстрел',
+            icon: 'http://...',
+            description: 'Двойной выстрел наносит двойной урон'
+        }, 
+        {
+            id: 9,
+            name: 'Нокаутирующий удар',
+            icon: 'http://...',
+            description: 'Описание недоступно'
+        }
+    ];
+    expect(received).toEqual(expected);
 })
+
